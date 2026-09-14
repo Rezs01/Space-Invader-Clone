f7eb6023c5bcc6657578b8c4ac52b5b0f8e1549e
Szer Dave Victorino <dave.victorino13@gmail.com>
2026-09-14 21:33:54 +0800

    Added rotation on ship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,13 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+
+    [Header("References")]
     [SerializeField] Transform bulletSpawnLocation;
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float shootInterval;
     [SerializeField] AudioClip shootSFX;
     [SerializeField] float shootVolume;
     [SerializeField] AudioClip deathSFX;
@@ -29,13 +34,25 @@ public class Player : MonoBehaviour
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
         //print(moveValue);
         transform.position += new Vector3(moveValue.x, 0, 0) * moveSpeed * Time.deltaTime;
+
+        //Rotate ship
+        if(transform.localEulerAngles.z < 70f || transform.localEulerAngles.z > 270f)
+        {
+            transform.RotateAround(transform.position, Vector3.forward, -moveValue.x * rotateSpeed * Time.deltaTime);
+        }
+
+        //Rotate ship back to 0 when not moving
+        //if (moveValue.x == 0) transform.RotateAround(transform.position, Vector3.forward, -transform.localEulerAngles.z * rotateSpeed * Time.deltaTime);
+        if (moveValue.x == 0) transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.identity, rotateSpeed * Time.deltaTime);
+        //print($"{moveValue.x}");
+        //print($"{transform.localEulerAngles.z}");
     }
 
     IEnumerator ShootLoop()
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(shootInterval);
             //print("Firing");
             Bullet bullet = BulletsPool.Instance.GetBullet();
             bullet.transform.SetPositionAndRotation(bulletSpawnLocation.position, new Quaternion(0, 0, 0, 1));
